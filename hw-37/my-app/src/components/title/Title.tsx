58400a9f33834ea8c89f482196ccad94d5eac1f5
Natalya <natalya.karaychentseva@gmail.com>
2022-04-10 22:21:41 +0300

    ht-40 add svg as react component

--- hw-37/my-app/src/components/title/Title.tsx
+++ hw-37/my-app/src/components/title/Title.tsx
@@ -1,11 +1,15 @@
 import "./Title.scss";
+import { ReactComponent as CatIcon} from "../../assets/cat.svg";
 
 const Title = () => {
 
     return (
-      <h1 className='title'>Make your blog <span className='title__item'>
-                
-        Online</span></h1>
+      
+      <h1 className='title'>
+        <CatIcon className='title__icon'/>
+        Make your blog <span className='title__item'> Online</span>
+        <CatIcon className='title__icon'/>
+      </h1>
     )
 }
 
