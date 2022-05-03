6c992d396db559c88b3c9c6a42f5a02d3ba9f415
Natalya <natalya.karaychentseva@gmail.com>
2022-05-03 15:42:07 +0300

    ht-47 - add reducer for post

--- hw-37/my-app/src/components/post/Post.tsx
+++ hw-37/my-app/src/components/post/Post.tsx
@@ -1,4 +1,7 @@
 import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useActions } from '../../hooks/useActions';
+import { useSelector } from '../../hooks/useSelector';
 import usePost from '../../hooks/usePost';
 import PostPreview from '../post-preview/PostPreview';
 
@@ -9,7 +12,15 @@ const URL = "https://studapi.teachmeskills.by/blog/posts/";
 
 const Post: React.FC<PropsType> = () => {
     const { id } = useParams();
-    const { data, loading, error } = usePost(id);
+    const data = useSelector(state => state.post.data);
+    const loading = useSelector(state => state.post.loading);
+    const error = useSelector(state => state.post.error);
+
+    const { fetchPost } = useActions();
+
+    useEffect(() => {
+        fetchPost(id);
+    }, [id]);
     
     if (loading) {
         return (
