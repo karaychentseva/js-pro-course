f4af45cbd70b606ef7a874ab9d15e000e2e13645
Natalya <natalya.karaychentseva@gmail.com>
2022-05-04 21:59:23 +0300

    ht-48 - use slice+thunk for posts

--- hw-37/my-app/src/hooks/usePost.ts
+++
@@ -1,9 +0,0 @@
-import PostType from '../types/PostType';
-import useRequest from './useRequest';
-
-const URL = "https://studapi.teachmeskills.by/blog/posts/";
-
-const usePost = (id: string | undefined) =>
-    useRequest<PostType | undefined>(undefined, `${URL}/${id}`);
-
-export default usePost;
--- hw-37/my-app/src/store/reducer.ts
+++ hw-37/my-app/src/store/reducer.ts
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import { clickerReducer } from "./clicker/reducer";
 import { postReducer } from "./post/reducer";
+import { postsReducer } from "./posts/postsSlice";
 
-export default combineReducers({
+const reducer = combineReducers({
     clicker: clickerReducer,
     post: postReducer,
-}); 
+    posts: postsReducer,
+});
+
+export default reducer;
